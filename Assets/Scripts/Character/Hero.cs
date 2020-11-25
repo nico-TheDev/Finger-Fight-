8c8b9bc3584567f4c5db93824bb8e502d552b7cf
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-25 23:52:18 +0800

    initial commit

---
+++ Assets/Scripts/Character/Hero.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+  public string heroName;
+
+  public int maxHealth;
+  public int currentHealth;
+  public int attackDamage;
+  public float critRate;
+  public float critDamage;
+}
