c9aec1aab7a6e00e87a60b63c26244f8d5f9c971
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-27 16:04:18 +0800

    add taking damage to hero function

--- Assets/Scripts/Character/Hero.cs
+++ Assets/Scripts/Character/Hero.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
 
-  public string heroName;
+    public string heroName;
 
-  public int maxHealth;
-  public int currentHealth;
-  public int attackDamage;
-  public float critRate;
-  public float critDamage;
+    public int maxHealth;
+    public int currentHealth;
+    public int attackDamage;
+    public float critRate;
+    public float critDamage;
+
+    public void TakeDamage(int damage)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth -= damage;
+
+    }
+
+    public int GetDamage(float percentage = 1f)
+    {
+
+        float critDmg = attackDamage * critRate;
+        float normalDmg = attackDamage * (1 - critRate);
+
+        return (int)((critDmg + normalDmg) * percentage);
+    }
 }
