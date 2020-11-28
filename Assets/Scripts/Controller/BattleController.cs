51aaebeb065aa4e9dfcc484e3b493b5b7e596614
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-28 19:44:05 +0800

    finish battle logic

--- Assets/Scripts/Controller/BattleController.cs
+++ Assets/Scripts/Controller/BattleController.cs
@@ -95,7 +95,7 @@ public class BattleController : MonoBehaviour
             {
                 enemyButtons.SetActive(false);
                 enemyTurnCount = 0;
-                StandOff();
+                StartCoroutine(StandOff());
             }
         }
         // ENEMY TURN
@@ -103,9 +103,66 @@ public class BattleController : MonoBehaviour
     }
 
 
-    void StandOff()
+    IEnumerator StandOff()
     {
         state = BattleState.STANDOFF;
+        yield return new WaitForSeconds(2f);
+
+        for (int i = 0; i < 3; i++)
+        {
+            string player = playerMoves[i];
+            string enemy = enemyMoves[i];
+
+
+            if (player == "Scissors")
+            {
+                if (enemy == "Scissors") BothDamage(50);
+                else if (enemy == "Rock") TwentyDamage(enemyHero, playerHero.GetDamage(0.2f));
+                else if (enemy == "Paper") FullDamage(enemyHero, playerHero.GetDamage());
+            }
+            else if (player == "Rock")
+            {
+                if (enemy == "Scissors") TwentyDamage(playerHero, enemyHero.GetDamage(0.2f));
+                else if (enemy == "Rock") print("DRAW");
+                else if (enemy == "Paper") FullDamage(playerHero, enemyHero.GetDamage());
+            }
+            else if (player == "Paper")
+            {
+                if (enemy == "Scissors") FullDamage(playerHero, enemyHero.GetDamage());
+                else if (enemy == "Rock") FullDamage(enemyHero, playerHero.GetDamage());
+                else if (enemy == "Paper") print("DRAW");
+            }
+
+
+            // SHOW ANIMATION OF CLASHING 
+
+            // ANNOUNCE WHAT HAPPENED
+
+            // UPDATE HUD
+            yield return new WaitForSeconds(0.4f);
+            playerHUD.SetHP(playerHero.currentHealth);
+            enemyHUD.SetHP(enemyHero.currentHealth);
+            // Check if there is a winner 
+            if (playerHero.currentHealth <= 0)
+            {
+                StartCoroutine(AnnounceResult(BattleState.LOST, "YOU LOSE!"));
+                break;
+            }
+            else if (enemyHero.currentHealth <= 0)
+            {
+                StartCoroutine(AnnounceResult(BattleState.WON, "YOU WIN!"));
+                break;
+            }
+
+
+            // Change State to Player TURN
+        }
+
+        if (state != BattleState.WON || state != BattleState.LOST)
+        {
+            state = BattleState.PLAYERTURN;
+            playerButtons.SetActive(true);
+        }
 
     }
 
@@ -122,10 +179,24 @@ public class BattleController : MonoBehaviour
         targetHero.TakeDamage(damage);
     }
 
-    void BothDamage(Hero currentHero, Hero enemyHero, int damage)
+    void BothDamage(int damage)
     {
-        currentHero.TakeDamage(damage);
+        playerHero.TakeDamage(damage);
         enemyHero.TakeDamage(damage);
+    }
+
+    IEnumerator AnnounceResult(BattleState finalState, string resultMsg)
+    {
+        state = finalState;
+        EndBattle();
+        print(resultMsg);
+        yield return new WaitForSeconds(2f);
+    }
+
+    void EndBattle()
+    {
+        Destroy(playerButtons);
+        Destroy(enemyButtons);
     }
 
     void Update()
