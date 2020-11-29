cd25f22d8f94c478651ae7221fbc6a5c728e59d7
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-29 11:17:45 +0800

    add initial cutscene controller

---
+++ Assets/Scripts/Controller/CutSceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutSceneController : MonoBehaviour
+{
+
+    public Sprite[] introStory = new Sprite[3];
+    public Sprite[] outroStory = new Sprite[3];
+
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    IEnumerator Activate()
+    {
+        yield return new WaitForSeconds(5f);
+        print("CLICK TO CONTINUE");
+
+    }
+}
