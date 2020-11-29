b39bee6f7e93dec22a1be32985aa0f56ba350499
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-29 11:17:58 +0800

    add scene controller

---
+++ Assets/Scripts/Controller/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void LoadSelect()
+    {
+        SceneManager.LoadScene("CharacterSelectScene");
+    }
+    public void LoadSelectPVP()
+    {
+        SceneManager.LoadScene("CharacterSelectScenePVP");
+    }
+    public void LoadBattle()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+    public void LoadBattlePVP()
+    {
+        SceneManager.LoadScene("BattleScenePVP");
+    }
+    public void LoadCutscene()
+    {
+        SceneManager.LoadScene("StoryScene");
+    }
+
+    public void LoadHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayScene");
+    }
+
+}
