751e7f08982e090c08099f5393af693097930214
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-01 20:05:08 +0800

    add change character

---
+++ Assets/Scripts/Controller/CharacterSelectController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterSelectController : MonoBehaviour
+{
+    public void SelectCharacter(){
+        string selected = EventSystem.current.currentSelectedGameObject.tag;
+        PlayerPrefs.SetString("playerChar",selected);
+        PlayerPrefs.SetString("CutsceneState","intro");
+    }
+}
