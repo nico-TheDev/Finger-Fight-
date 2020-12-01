366a5f1a100d74cb12384599b9e2fa847bb64d45
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-01 20:05:24 +0800

    add cutscene based on character

--- Assets/Scripts/Controller/CutSceneController.cs
+++ Assets/Scripts/Controller/CutSceneController.cs
@@ -14,13 +14,35 @@ public class CutSceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Alien 
+        // Knight
+        // Samurai
+        button.enabled = false;
+        Image btnImage = button.GetComponent<Image>();
+        string currentState = PlayerPrefs.GetString("CutsceneState");
+        string playerChar = PlayerPrefs.GetString("playerChar");
 
+        if (currentState == "intro")
+        {
+            if (playerChar == "Alien") btnImage.sprite = introStory[0];
+            else if (playerChar == "Knight") btnImage.sprite = introStory[1];
+            else if (playerChar == "Samurai") btnImage.sprite = introStory[2];
+        }
+        else if (currentState == "outro")
+        {
+            if (playerChar == "Alien") btnImage.sprite = outroStory[0];
+            else if (playerChar == "Knight") btnImage.sprite = outroStory[1];
+            else if (playerChar == "Samurai") btnImage.sprite = outroStory[2];
+        }
+
+        StartCoroutine(Activate());
     }
 
     IEnumerator Activate()
     {
         yield return new WaitForSeconds(5f);
         print("CLICK TO CONTINUE");
+        button.enabled = true;
 
     }
 }
