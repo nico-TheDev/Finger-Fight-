9391bded6414f26fdfb0622e0ef11a63c301d11f
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-04 19:12:58 +0800

    add button sfx

---
+++ Assets/Scripts/Controller/ButtonFxController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFxController : MonoBehaviour
+{
+    AudioSource btnFx;
+
+    public AudioClip clickFx;
+
+    void Start()
+    {
+        btnFx = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void ClickFX()
+    {
+        btnFx.PlayOneShot(clickFx);
+    }
+}
