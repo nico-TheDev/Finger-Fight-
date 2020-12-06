1a14d5d5be41e22a1cde4189094a8ed4e8c55cea
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-06 17:23:00 +0800

    add versus controller

---
+++ Assets/Scripts/Controller/VersusController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersusController : MonoBehaviour
+{
+    public Image player;
+    public Image enemy;
+
+    public SceneController scene;
+
+    public Sprite[] characters = new Sprite[3];
+    void Start()
+    {
+        string[] chars = { "Knight", "Samurai", "Alien" };
+        string enemyChar = chars[PlayerPrefs.GetInt("currentStage")];
+        string playerChar = PlayerPrefs.GetString("playerChar");
+
+        GetCharacter(enemy, enemyChar);
+        GetCharacter(player, playerChar);
+
+        StartCoroutine(Battle());
+    }
+
+
+    IEnumerator Battle()
+    {
+        yield return new WaitForSeconds(3f);
+        scene.LoadBattle();
+    }
+
+    void GetCharacter(Image cur, string character)
+    {
+        if (character == "Knight")
+        {
+            cur.sprite = characters[0];
+        }
+        else if (character == "Samurai")
+        {
+            cur.sprite = characters[1];
+
+        }
+        else if (character == "Alien")
+        {
+            cur.sprite = characters[2];
+        }
+    }
+}
