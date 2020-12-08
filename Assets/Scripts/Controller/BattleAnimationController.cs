6edd7ebf1d03039ae80e6cf6680a07c540412062
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-08 20:48:35 +0800

    add intro anims

---
+++ Assets/Scripts/Controller/BattleAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleAnimationController : MonoBehaviour
+{
+    public Image roundFight;
+    public Image roundFinger;
+    public Image knockout;
+
+    public Animator fightIntro;
+    public Animator fingerIntro;
+
+    public Animator knockoutIntro;
+    public void PlayFinger()
+    {
+        fingerIntro.SetTrigger("isPlaying");
+        FindObjectOfType<AudioManager>().Play("FingerIntro");
+    }
+    public void PlayFight()
+    {
+        fightIntro.SetTrigger("isPlaying");
+        FindObjectOfType<AudioManager>().Play("FightIntro");
+    }
+    public void PlayKO()
+    {
+        knockoutIntro.SetTrigger("isPlaying");
+        FindObjectOfType<AudioManager>().Play("Knockout");
+    }
+
+}
