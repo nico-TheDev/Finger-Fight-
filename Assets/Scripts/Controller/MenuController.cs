8997acd63b447696ca15f39b401fc45f472b0b9d
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-19 20:36:38 +0800

    menu and how to play controller

---
+++ Assets/Scripts/Controller/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject PanelOne;
+    public GameObject PanelTwo;
+
+    public void ShowPanelOne()
+    {
+        PanelOne.SetActive(true);
+        PanelTwo.SetActive(false);
+    }
+    public void ShowPanelTwo()
+    {
+        PanelOne.SetActive(false);
+        PanelTwo.SetActive(true);
+    }
+}
