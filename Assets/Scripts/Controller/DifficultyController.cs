139bbba8fa15adf77771ff5e892188d07daa5118
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-12-19 20:36:06 +0800

    addd difficulty control

---
+++ Assets/Scripts/Controller/DifficultyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyController : MonoBehaviour
+{
+
+    SceneController sceneManager;
+
+    void Start()
+    {
+        sceneManager = GetComponent<SceneController>();
+    }
+    void SetDifficulty(string diff)
+    {
+        if (diff == "easy") PlayerPrefs.SetFloat("difficulty", 1f);
+        else if (diff == "medium") PlayerPrefs.SetFloat("difficulty", 1.25f);
+        else if (diff == "hard") PlayerPrefs.SetFloat("difficulty", 1.5f);
+
+        sceneManager.LoadSelect();
+    }
+
+    public void setEasy()
+    {
+        SetDifficulty("easy");
+    }
+    public void setMedium()
+    {
+        SetDifficulty("medium");
+    }
+    public void setHard()
+    {
+        SetDifficulty("hard");
+    }
+}
